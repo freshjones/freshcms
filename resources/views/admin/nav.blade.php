a96a78e6ef250a0155eb5498598f78fe6bfa5e63
William Jones <billy@freshjones.com>
2018-07-03 13:50:03 -0400

    comment out admin navigation

--- resources/views/admin/nav.blade.php
+++ resources/views/admin/nav.blade.php
@@ -12,21 +12,21 @@
         <div class="dropdown-toggle" id="dropdownMenuLink" data-toggle="dropdown">Settings</div>
         <ul class="dropdown-menu dropdown-menu-dark settings-menu" role="menu">
           <li class="dropdown-header">Account</li>
-          <li><a href="site.list" >Sites &amp; Organizations</a></li>
-          <li><a href="user.list">Users</a></li>
-          <li><a href="/settings/ip">IP Blocking</a></li>
+          {{-- <li><a href="site.list" >Sites &amp; Organizations</a></li> --}}
+          {{-- <li><a href="user.list">Users</a></li> --}}
+          {{-- <li><a href="/settings/ip">IP Blocking</a></li> --}}
           {{-- onClick="event.preventDefault(); app.$refs.settings.toggle()" --}}
           <li><a href="{{ route('settings-configuration') }}" >Configuration</a></li>
-          <li><a href="/settings/integrations">Integrations</a></li>
-          <li><a href="/settings/visitor-lookup">Visitor Lookup</a></li>
-          <li class="dropdown-header">Billing</li>
-          <li><a href="/settings/billing">Plans &amp; Billing</a></li>
-          <li><a href="/settings/invoices">Invoices</a></li>
-          <li class="dropdown-header ellipsis">{{ Auth::user()->email }}</li>
-          <li><a href="/profile/details">My Details</a></li>
-          <li><a href="/profile/notifications">Notifications</a></li>
-          <li><a href="profile.referrals">Refer Friends</a></li>
-          <li><a class="danger">Logout</a></li>
+          {{-- <li><a href="/settings/integrations">Integrations</a></li> --}}
+          {{-- <li><a href="/settings/visitor-lookup">Visitor Lookup</a></li> --}}
+          {{-- <li class="dropdown-header">Billing</li> --}}
+          {{-- <li><a href="/settings/billing">Plans &amp; Billing</a></li> --}}
+          {{-- <li><a href="/settings/invoices">Invoices</a></li> --}}
+          {{-- <li class="dropdown-header ellipsis">{{ Auth::user()->email }}</li> --}}
+          {{-- <li><a href="/profile/details">My Details</a></li> --}}
+          {{-- <li><a href="/profile/notifications">Notifications</a></li> --}}
+          {{-- <li><a href="profile.referrals">Refer Friends</a></li> --}}
+          {{-- <li><a class="danger">Logout</a></li> --}}
         </ul>
       </li>
       <li class="py-1 px-2">
