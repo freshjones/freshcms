0f1afb72719d9bdcb3cfc93a573fc42a9e3a0b6a
William Jones <billy@freshjones.com>
2018-07-03 12:05:44 -0400

    routes separation

--- routes/web.php
+++ routes/web.php
@@ -3,20 +3,20 @@
 Auth::routes();
 
 /* setting routes */
-require_once(__DIR__.'/web_routes/setting_routes.php');
+require(__DIR__.'/web_routes/setting_routes.php');
 
 /* section routes */
-require_once(__DIR__.'/web_routes/section_routes.php');
+require(__DIR__.'/web_routes/section_routes.php');
 
 /* content routes */
-require_once(__DIR__.'/web_routes/content_routes.php');
+require(__DIR__.'/web_routes/content_routes.php');
 
 /* billboard routes */
-require_once(__DIR__.'/web_routes/billboard_routes.php');
-require_once(__DIR__.'/web_routes/billboard_screen_routes.php');
+require(__DIR__.'/web_routes/billboard_routes.php');
+require(__DIR__.'/web_routes/billboard_screen_routes.php');
 
 /* page routes */
-require_once(__DIR__.'/web_routes/page_routes.php');
+require(__DIR__.'/web_routes/page_routes.php');
 
 
 
