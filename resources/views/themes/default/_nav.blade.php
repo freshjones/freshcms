3b8431ff35dfc290cb07a9bb1634eb2aa353c625
William Jones <billy@freshjones.com>
2018-07-03 17:05:27 -0400

    adding nav

---
+++ resources/views/themes/default/_nav.blade.php
@@ -0,0 +1,37 @@
+<ul class="navbar-nav">
+    <li class="nav-item">
+        <a class="nav-link" href="{{ route('home') }}">Home</a>
+    </li>
+    {{--
+    <li class="nav-item dropdown">
+        <a class="nav-link dropdown-toggle" href="#" id="navbarSolutions" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Solutions</a>
+        <div class="dropdown-menu" aria-labelledby="navbarSolutions">
+            <a class="dropdown-item" href="#">Action</a>
+            <a class="dropdown-item" href="#">Another action</a>
+            <div class="dropdown-divider"></div>
+            <a class="dropdown-item" href="#">Something else here</a>
+        </div>
+    </li>
+    <li class="nav-item dropdown">
+        <a class="nav-link dropdown-toggle" href="#" id="navbarServices" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Services</a>
+        <div class="dropdown-menu" aria-labelledby="navbarServices">
+            <a class="dropdown-item" href="#">Action</a>
+            <a class="dropdown-item" href="#">Another action</a>
+            <div class="dropdown-divider"></div>
+            <a class="dropdown-item" href="#">Something else here</a>
+        </div>
+    </li>
+    <li class="nav-item">
+        <a class="nav-link" href="#">Open Y</a>
+    </li>
+    <li class="nav-item">
+        <a class="nav-link" href="#">About</a>
+    </li>
+    <li class="nav-item">
+        <a class="nav-link" href="#">Blog</a>
+    </li>
+    <li class="nav-item">
+        <a class="nav-link" href="#">Contact</a>
+    </li>
+    --}}
+</ul>
