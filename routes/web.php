d086090d1724a7ec3deb2dd4cff8c561ca3ae840
William Jones <billy@freshjones.com>
2018-07-19 13:03:15 -0400

    dialogflow

--- routes/web.php
+++ routes/web.php
@@ -23,17 +23,12 @@ function getMessages()
 {
     $json = '[
     {
-      "card": {
-        "title": "card title",
-        "subtitle": "card text",
-        "imageUri": "https://assistant.google.com/static/images/molecule/Molecule-Formation-stop.png",
-        "buttons": [
-          {
-            "text": "button text",
-            "postback": "https://assistant.google.com/"
-          }
-        ]
-      }
+        "text": {
+          "text": [
+            "Text defined in Dialogflows console for the intent that was matched",
+            "Text defined in Dialogflows console for the intent that was matched"
+          ]
+        }
     }
   ]';
 
