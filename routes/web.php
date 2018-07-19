71222f3d705a3c043080bf607d1e1fb2761c18d7
William Jones <billy@freshjones.com>
2018-07-19 11:36:37 -0400

    dialogflow

--- routes/web.php
+++ routes/web.php
@@ -31,13 +31,12 @@ Route::post('/dialogflow/demo', function(\Illuminate\Http\Request $request){
     $fixture = '{"responseId":"832f3a5a-1ee4-4ed8-9367-dbe1d95b152e","queryResult":{"queryText":"yes","parameters":[],"allRequiredParamsPresent":true,"fulfillmentMessages":[{"text":{"text":[null]}}],"intent":{"name":"projects\/ymcademo-v2\/agent\/intents\/e881bba8-6dbf-4f0a-a6b7-ae03ec30e619","displayName":"Ask-findclass"},"intentDetectionConfidence":1,"languageCode":"en"},"originalDetectIntentRequest":{"payload":[]},"session":"projects\/ymcademo-v2\/agent\/sessions\/53e7d232-2883-7967-0e64-44c41f5d1faf"}';
 
     $data = json_decode($fixture);
-
+    */
 
     $file = __DIR__ . '/../demo.txt';
 
-    file_put_contents($file, json_encode($request->all()) );
-
-    */
+    file_put_contents($file, print_r($request->all(),true) );
+  
 
     $intent = $request->queryResult['intent']['displayName'];
     $session = $request->session;
@@ -49,6 +48,11 @@ Route::post('/dialogflow/demo', function(\Illuminate\Http\Request $request){
         case 'Ask-findclass':
             $message = 'OK, Lets get started, what YMCA do you use?';
         break;
+
+        case 'Ask-location':
+            $message = 'Excellent, Are you looking for a class this fall?';
+        break;
+
     }
 
     return [
